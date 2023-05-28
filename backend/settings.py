37d1f6611ca23cffeb23e5b481e5069fb05d7e3b
aeroajith <nexusrobotech2022@email.com>
2023-05-28 17:41:12 +0530

    eight

--- backend/settings.py
+++ backend/settings.py
@@ -145,7 +145,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,'medicalapp/build/static')]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'build/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
    
 
 
