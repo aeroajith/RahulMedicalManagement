3dcd1063a55c7809a1d3482d55b06f04d25b24c7
aeroajith <nexusrobotech2022@email.com>
2023-05-30 08:27:15 +0530

    twntytwo

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u6z2_!g%*22$m^xmhh1z$p0odvuz+mls1=5$%4$#+c^!9oj%g%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['http://44.203.141.46/','*']
 
 
 # Application definition
