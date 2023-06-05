bc6c3ae871401102373ae90af223719c098a7486
aeroajith <nexusrobotech2022@email.com>
2023-06-05 15:16:09 +0530

    foutyeight

--- backend/settings.py
+++ backend/settings.py
@@ -170,7 +170,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "http://ragavimedical.online",
-    "http://127.0.0.1:8000/"
+    "http://127.0.0.1:8000"
 ]
 
 CSRF_COOKIE_SECURE = False
